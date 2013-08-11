e7e53a080c9117b1a63895a365a4910ee992ce29
petredy <petredy@petredy-desktop>
2013-08-11 19:24:08 +0200

    Initialised client, server

--- src/crocking/client/Main.java
+++ src/crocking/client/Main.java
@@ -4,9 +4,14 @@ import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
+import com.jme3.network.Client;
+import com.jme3.network.Network;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * test
@@ -14,6 +19,8 @@ import com.jme3.scene.shape.Box;
  */
 public class Main extends SimpleApplication {
 
+    public static Client client;
+    
     public static void main(String[] args) {
         Main app = new Main();
         app.start();
@@ -21,14 +28,12 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(Vector3f.ZERO, 1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+        try {
+            client = Network.connectToServer("localhost", 6413);
+            client.start();
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
--- src/crocking/server/Main.java
+++ src/crocking/server/Main.java
@@ -5,9 +5,15 @@ import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
+import com.jme3.network.Network;
+import com.jme3.network.Server;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
+import com.jme3.system.JmeContext;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * test
@@ -15,21 +21,23 @@ import com.jme3.scene.shape.Box;
  */
 public class Main extends SimpleApplication {
 
+    
+    public static int port = 6413;
+    public static Server server;
+    
     public static void main(String[] args) {
         Main app = new Main();
-        app.start();
+        app.start(JmeContext.Type.Headless);
     }
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(Vector3f.ZERO, 1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+        try {
+            server = Network.createServer(port);
+            server.start();
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
